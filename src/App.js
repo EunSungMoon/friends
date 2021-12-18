cf89af331554a9240ae40d3df7486344f79165ca
Eunsung Moon <eunsung9647@gmail.com>
2021-12-18 20:19:42 +0900

    프렌즈 기반

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import { FaBeer } from 'react-icons/fa';
+
+function App() {
+  return (
+    <div className="App">
+      <FaBeer />
+    </div>
+  );
+}
+
+export default App;
