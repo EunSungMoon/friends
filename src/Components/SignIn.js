f0c0a32e5c0badba1c7a615dcd67d73ba23d9994
Eunsung Moon <eunsung9647@gmail.com>
2021-12-31 17:06:18 +0900

    회원가입 페이지 4차

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,42 +1,48 @@
 import './style/SignIn.scss';
 import { useState } from 'react';
 import InputCom from './InputCom';
+import useForm from '../Hooks/useForm';
+import validate from './validate';
 
 export default function SignIn(props) {
 
+  const { values, errors, submitting, handleChange, handleSubmit } = useForm({
+    initialValues: { email: '', password: '', nickName: '' },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 3));
+    },
+    validate
+  })
+
+  const [visible, setVisible] = useState(false);
   const [reason, setReason] = useState('volunteer');
-  const [visible, setVisible] = useState(false)
+
   const [password, setPassword] = useState('');
-  // const [isPassword, setIsPassword] = useState(false)
-  const [passwordError, setPasswordError] = useState(false);
-  const [setPasswordCheck] = useState('');
+
+  const [noMatchPassword, setNoMatchPassword] = useState(false);
+  const [passwordCheck, setPasswordCheck] = useState('');
 
 
-  const handleChange = e => {
+  const handleReason = e => {
     setReason(e.currentTarget.value);
     setVisible(!visible)
   }
 
-  const onChangePassword = e => {
-    setPassword(e.currentTarget.value)
+  const handlePassword = e => {
+    setPassword(e.target.value)
   }
 
-  const onChangePasswordChk = (e) => {
-    setPasswordError(e.target.value !== password);
+  const handlePasswordChk = (e) => {
+    setNoMatchPassword(e.target.value !== values.password);
     setPasswordCheck(e.target.value);
   };
-
-  // const checkPassword = () => {
-    
-  // }
-
 
   return (
     <main id="sign-main">
       <div className="container">
         <h2 className='h2'>회원가입</h2>
         <section className='section'>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className='email-input form-common'>
               <InputCom
                 title="이메일"
@@ -44,37 +50,49 @@ export default function SignIn(props) {
                 type="text"
                 class="text-input longWidth"
                 placeholder="이메일주소를 입력해주세요."
+                event={handleChange}
+                value={values.email}
               />
+              {errors.email && <p style={{ color: 'red' }}>{errors.email}</p>}
             </div>
             <div className='password-input form-common'>
               <InputCom
                 title="비밀번호"
                 name="password"
                 type="password"
-                class="text-input longWidth pw"
+                class="text-input longWidth"
                 placeholder="비밀번호를 입력해주세요."
-                event={onChangePassword}
+                event={handleChange}
+                onChange={handlePassword}
+                value={values.password}
               />
+              {errors.password && <p style={{ color: 'red' }}>{errors.password}</p>}
+            </div>
+            <div className='password-input form-common'>
               <InputCom
                 title="비밀번호 재확인"
                 name="password"
                 type="password"
                 class="text-input longWidth"
                 placeholder="비밀번호를 입력해주세요."
-                event={onChangePasswordChk}
+                event={handlePasswordChk}
               />
-              {passwordError && <p style={{ color: 'red' }}>비밀번호가 일치하지 않습니다.</p>}
+              {noMatchPassword && <p style={{ color: 'red' }}>비밀번호가 일치하지 않습니다.</p>}
             </div>
             <div className='nickname-input form-common'>
               <InputCom
                 title="별명"
-                name="nickname"
+                name="nickName"
                 type="text"
                 class="text-input nickname"
                 placeholder="별명을 입력해주세요."
+                event={handleChange}
+                value={values.nickName}
               />
               <button type='button' className='confirmBtn'>중복확인</button>
+              {errors.nickName && <p style={{ color: 'red' }}>{errors.nickName}</p>}
             </div>
+
             <div className='join-reason form-common'>
               <p>가입이유</p>
               <label className='volunteer'>
@@ -83,7 +101,7 @@ export default function SignIn(props) {
                   name='join'
                   value='volunteer'
                   checked={reason === 'volunteer'}
-                  onChange={handleChange}
+                  onChange={handleReason}
                 />봉사활동을 하고 싶어요
               </label>
               <label className='need-helper'>
@@ -92,7 +110,7 @@ export default function SignIn(props) {
                   name='join'
                   value='need-helper'
                   checked={reason === 'need-helper'}
-                  onChange={handleChange}
+                  onChange={handleReason}
                 />봉사자의 도움이 필요해요
               </label>
             </div>
@@ -108,9 +126,10 @@ export default function SignIn(props) {
                 />
               </div>
             }
+
+            <p className='infoText'>회원가입 시 이메일 인증 메일이 발송됩니다. <br />정확한 이메일 주소를 입력해주세요.</p>
+            <button type='submit' className='joinComplete' disabled={submitting} >회원가입</button>
           </form>
-          <p className='infoText'>회원가입 시 이메일 인증 메일이 발송됩니다. <br />정확한 이메일 주소를 입력해주세요.</p>
-          <button className='joinComplete'>가입완료</button>
         </section>
       </div>
     </main>
