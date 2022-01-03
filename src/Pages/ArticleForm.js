989e84e66ff1870d34debbb00e597e4e9a17e136
Eunsung Moon <eunsung9647@gmail.com>
2022-01-03 19:56:04 +0900

    봉사등록 페이지 리팩토링

--- src/Pages/ArticleForm.js
+++ src/Pages/ArticleForm.js
@@ -70,50 +70,6 @@ export default function ArticleForm() {
               </div>
             </div>
           </form>
-          {/* <table>
-            <colgroup>
-              <col style={{ width: '150px' }}></col>
-              <col></col>
-            </colgroup>
-            <tbody>
-              <tr>
-                <th>글 제목</th>
-                <td><input type='text' className='article-input' placeholder='제목을 입력해주세요.' /></td>
-              </tr>
-              <tr>
-                <th>봉사 일</th>
-                <td>
-                  <DatePicker
-                    selected={startDate}
-                    onChange={(date) => setStartDate(date)}
-                    locale={ko}
-                    showTimeSelect
-                    timeFormat='HH:mm'
-                    timeCaption='time'
-                    dateFormat='yyyy년 MM월 dd일 aa h시 mm분'
-                  />
-                </td>
-              </tr>
-              <tr>
-                <th>봉사 인원</th>
-                <td><NumberCountCom /></td>
-              </tr>
-              <tr>
-                <th>봉사분야</th>
-                <td>
-                  <SelectBoxCom options={OPTIONS} defaultValue="haircut" />
-                </td>
-              </tr>
-              <tr>
-                <th>봉사 장소</th>
-                <td><AddressCom /></td>
-              </tr>
-              <tr>
-                <th>상세내용</th>
-                <td><textarea placeholder='상세내용' className='textarea'></textarea></td>
-              </tr>
-            </tbody>
-          </table> */}
         </section>
       </div>
     </main>
