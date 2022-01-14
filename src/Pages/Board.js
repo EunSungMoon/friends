485682d90234aff1f0ee9e745a28ed16c46cd166
Eunsung Moon <eunsung9647@gmail.com>
2022-01-14 11:25:00 +0900

    board axios로 수정

--- src/Pages/Board.js
+++ src/Pages/Board.js
@@ -3,42 +3,41 @@ import { BsPencil, BsCalendarDate, BsPeople, BsShopWindow } from "react-icons/bs
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import LogInModal from '../Components/LogInModal';
+import axios from 'axios'
 
 export default function Board() {
-
-  const [users, setUsers] = useState(null);
+  const [lists, setLists] = useState([])
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [loginModalShow, setloginModalShow] = useState(false);
 
-  const loadFetch = () => {
-    fetch(`http://15.164.62.156:8888/api/board`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    })
-      .then((response) => {
-        return response.json()
+  const loadAxios = async () => {
+    try {
+      setError(null);
+      setLists(null);
+      setLoading(true);
+      const loadData = await axios.get('http://15.164.62.156:8888/api/board', {
+        headers: {
+          'Content-Type': 'application/json'
+        }
       })
-      .then(data => {
-        setUsers(data);
-      })
-      .catch(error => {
-        setError(error)
-      })
-    setLoading(false);
+      setLists(loadData.data)
+    }
+    catch(e) {
+      setError(e)
+    }
+    setLoading(false)
   }
 
   useEffect(() => {
-    loadFetch()
-    return users
+    loadAxios()
+    return lists
   }, []);
 
 
   if (loading) return <div>로딩중...</div>
   if (error) return <div>에러가 발생했습니다.</div>
-  if (!users) return null;
+  if (!lists) return null;
 
   return (
     <main id="board-main">
@@ -71,18 +70,18 @@ export default function Board() {
         <section className="container">
           <ol className="contentWrap row">
 
-            {users.map(user => (
-              <li className="col-6 list" data-type={user.id} key={user.id}>
-                <Link to={`/board/${user.id}/`}>
+            {lists.map(list => (
+              <li className="col-6 list" data-type={list.id} key={list.id}>
+                <Link to={`/board/${list.id}/`}>
                   <div className="listTitle">
-                    <h3 className="h3">{user.title}</h3>
-                    <p className={user.state}>모집중</p>
+                    <h3 className="h3">{list.title}</h3>
+                    <p className={list.state}>모집중</p>
                   </div>
                   <div className="listContent">
-                    <p className="field">{user.part}</p>
-                    <p className="date"><BsCalendarDate className="bi" />{user.dday}</p>
-                    <p className="headcount"><BsPeople className="bi" />{user.members}명</p>
-                    <p className="address"><BsShopWindow className="bi" />{user.place}</p>
+                    <p className="field">{list.part}</p>
+                    <p className="date"><BsCalendarDate className="bi" />{list.dday}</p>
+                    <p className="headcount"><BsPeople className="bi" />{list.members}명</p>
+                    <p className="address"><BsShopWindow className="bi" />{list.roadAddress} {list.detailAddress}</p>
                   </div>
                 </Link>
               </li>
